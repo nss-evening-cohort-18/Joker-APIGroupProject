dc618c103390e6d2ff08f3476c078c534e79a82c
Chris <chrisl.brannon@gmail.com>
2022-08-23 16:51:01 -0700

    Spy Model filled out.

--- Joker-APIGroupProject/Models/Spy.cs
+++ Joker-APIGroupProject/Models/Spy.cs
@@ -2,5 +2,16 @@
 {
     public class Spy
     {
+        public int id { get; set; }
+        public string userName { get; set; }
+        public List<string> skills { get; set; }
+        public List<string> friends { get; set; }
+        public List<string> enemies { get; set; }
+        public string originStory { get; set; }
+        public List <string> services { get; set; }
+
+
+
+
     }
 }
