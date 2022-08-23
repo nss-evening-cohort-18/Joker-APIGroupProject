b74f16ee50f9ece00f5970cb41e0681c052f8d53
VERNON WESTMORELAND <vfwestmoreland@gmail.com>
2022-08-23 18:51:53 -0500

    Initial Spy Repo

---
+++ Joker-APIGroupProject/Repositories/SpyRepository.cs
@@ -0,0 +1,17 @@
+﻿using Joker_APIGroupProject.Models;
+using Joker_APIGroupProject.Interface;
+
+
+namespace Joker_APIGroupProject.Repositories
+{
+    public class SpyRepository : ISpy
+    {
+        private static List<Spy> _spies = new()
+        {
+            new Spy()
+            {
+
+            }
+        }
+    }
+}
